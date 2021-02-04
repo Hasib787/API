b2af942bd8a48e5fb5fe309efac56633d1b39dfa
Hasib787 <mr.hasib45@gmail.com>
2021-02-04 22:15:51 +0600

    added a fucntion to load data form the server

--- js/jsonApi.js
+++ js/jsonApi.js
@@ -1,17 +1,35 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => displayUser(json))
+document.getElementById('submit').addEventListener('click', function(){
+    const title = document.getElementById('title').value;
+    const bodyContent = document.getElementById('bodyContent').value;
+    const post = {title: title, body: bodyContent};
+    nowPostToServer(post);
+
+})
+//load data from server
+function nowPostToServer(postInfo) {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(postInfo),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+
+}
+
 
 function displayUser(users) {
     const userNames = users.map(user => user.username);
-    const ul =document.getElementById('user-container');
+    const ul = document.getElementById('user-container');
 
     for (let i = 0; i < userNames.length; i++) {
         const username = userNames[i];
-        const li= document.createElement("li");
+        const li = document.createElement("li");
         li.innerText = username;
         ul.appendChild(li);
-        
+
     }
     console.log(userNames);
 }
